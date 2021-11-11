064f52b45f2c1d64363ef8e5b730822fc86e41e8
wxydejoy <1798923421@qq.com>
2021-11-11 20:50:19 +0800

    dds

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 import subprocess as sp
 
-command = "n pm version patch"             
+command = "npm version patch"             
 p = sp.Popen(command, shell=True,encoding="UTF-8",stdout=sp.PIPE,stderr=sp.PIPE)
 
 p.wait(100)
